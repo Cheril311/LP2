372aaa0d78c5d56434d421fe1015e0e017fe0a57
Cheril Shah <79891166+Cheril311@users.noreply.github.com>
2023-05-23 21:10:45 +0530

    Update server.js

--- WAD/W-5/server.js
+++ WAD/W-5/server.js
@@ -1,123 +1,135 @@
-import express from "express";
-import mongoose from "mongoose";
-
-import { songdetails } from "./data.js";
+const express = require("express");
+const mongoose = require("mongoose");
+const songSchema = require("./songDetails.js");
 
 const app = express();
-const port = 4000;
+
 app.use(express.json());
 
-mongoose
-  .connect(
-    "mongodb://localhost:27017",
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
-  .then(() => {
-    console.log("DataBase Connected");
-  });
 
-const songSchema = new mongoose.Schema({
-  Song: String,
-  Film: String,
-  Music_Director: String,
-  Singer: String,
-  Actor: String,
-  Actress: String,
+// TEST
+app.listen(8000, () => {
+    console.log("Server live");
 });
 
-const Song = mongoose.model("Song", songSchema);
-
-app.get("/", async (req, res) => {
-  try {
-    res.send("Server running on the port");
-  } catch {
-    return "Failure";
-  }
+app.get("/", (req, res) => {
+    res.status(200).send("This is WAD5 Server");
 });
 
-app.get("/adddata", async (req, res) => {
-    try{    
-        const song = await Song.insertMany(songdetails);
-        if (song){
-            res.send("Data added")
-        }
-        else
-        {
-            res.send("Data not added")
-        }
-    }
-    catch(err){
-        res.send(err)
-    }
-})
 
-app.get("/count", async (req, res) => {
-    try{    
-        const song = await Song.countDocuments(songdetails);
-        res.send(`Total Count: ${song}`)
-    }
-    catch(err){
-        res.send(err)
-    }
-})
+// ConnectDatabase
+const connectDatabase = () => {
+    var url = "mongodb://localhost:27017";
+    mongoose
+        .connect(url, { useNewUrlParser: true, useUnifiedTopology: true },)
+        .then(() => console.log('MongoDB connected'))
+        .catch((err) => console.log(err));
+}
+connectDatabase();
 
-app.get("/music_director", async (req, res) => {
-    try{    
-        const song = await Song.find({Music_Director: "Pritam"});
-        const songname =  song.map((songs) => songs.Song)
-        res.send(songname)
-    }
-    catch(err){
-        res.send(err)
-    }
-})
 
-app.get("/musicdir_singer", async (req, res) => {
-    try{    
-        // const musicdirec = res.params.Music_Director
-        // const singer = res.paramas.Singer
-        const song = await Song.find({Music_Director: "Pritam", Singer: "Manas"});
-        const songname =  song.map((songs) => songs.Song)
-        console.log(songname)
-        res.send(songname)
+// Operations at endpoints
+app.post("/c/", async (req, res) => {
+    try {
+        let dataToStore = await songSchema.create(req.body);
+        dataToStore = dataToStore.toObject();
+        res.status(200).json({ "Data": dataToStore });
+    } catch (error) {
+        res.status(500).send(error);
     }
-    catch(err){
-        res.send(err)
-    }
-})
+});
 
-app.get("/film_singer", async (req, res) => {
-    try{    
-        
-        const song = await Song.find({Film: "Tamasha", Singer: "Adi"});
-        console.log(song)
-        const songname =  song.map((songs) => songs.Song)
-        res.send(songname)
+app.get("/d/", async(req, res) => {
+    try {
+        let data = await songSchema.find();
+        res.status(200).json({"Size" : data.length, "Data" : data});
+    } catch (error) {
+        res.status(500).send(error);
     }
-    catch(err){
-        res.send(err)
-    }
-})
+});
 
-app.delete("/delete", async (req, res) => 
-{
-    try{
-        const songname = res.params.Song
-        const song = await Song.deleteOne({Song: songname})
-        if(song)
-        {
-            res.send("deleted")
-        }
-        else{
-            res.send("Cannot delete")
-        }
+app.get("/e/:director", async (req, res) => {
+    try {
+        let director = req.params.director;
+        const query = {Music_director: director};
+        let data = await songSchema.find(query);
+        res.status(200).send(data);
+    } catch (error) {
+        res.status(500).send(error);
     }
-    catch(err)
-    {
-        res.send(err)
-    }
-})
+});
 
-app.listen(port, () => {
-  console.log(`server running on ${port}`);
+app.get("/f/:director/:singer", async (req, res) => {
+    try {
+        let director = req.params.director;
+        let singer = req.params.singer;
+        const query = {Music_director: director, Singer:singer};
+        let data = await songSchema.find(query);
+        res.status(200).send(data);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+app.get("/g/:id", async (req, res) => {
+    try {
+        let id = req.params.id;
+        let data = await songSchema.findByIdAndDelete(id);
+        res.status(200).send(data);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+app.get("/i/:singer/:film", async (req, res) => {
+    try {
+        let film = req.params.film;
+        let singer = req.params.singer;
+        const query = {Film: film, Singer:singer};
+        let data = await songSchema.find(query);
+        res.status(200).send(data);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+app.put("/j/:id", async (req, res) => {
+    try {
+        let id = req.params.id;
+        let data = await songSchema.findOneAndUpdate({_id:id}, {$set: {Actor:req.body.Actor, Actress:req.body.Actress}}, {new:true});
+        data = data.toObject();
+        res.status(200).json({ "Updated Data": data});
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+
+app.get("/k/", async (req, res) => {
+    try {
+        let data = await songSchema.find();
+        let html = "<table border=1 style='border-collapse: collapse'>";
+        html = html + `<tr> <th> Song Name</th> <th> Film Name</th> <th> Music Director</th> <th> Singer </th> <th> Actor</th><th> Actress</th> </tr>`
+        data.map(function(song) {
+            if(song.Actor == null) {
+                song.Actor = "-";
+            }
+            if(song.Actress == null) {
+                song.Actress = "-";
+            }
+            html = html + "<tr>";
+            html = html + "<td>" + song.Songname + "</td>";
+            html = html + "<td>" + song.Film + "</td>";
+            html = html + "<td>" + song.Music_director + "</td>";
+            html = html + "<td>" + song.Singer + "</td>";
+            html = html + "<td>" + song.Actor + "</td>";
+            html = html + "<td>" + song.Actress + "</td>";
+            html = html + "</tr>";
+        });
+        html = html + "</table>";
+
+        res.send(html);
+    } catch (error) {
+        res.status(500).send(error);
+    }
 });
