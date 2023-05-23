b0fdf2014c2c8efd05bb5e6e114cd38b22857b93
Cheril Shah <79891166+Cheril311@users.noreply.github.com>
2023-05-23 21:11:12 +0530

    Update server.js

--- WAD/W-5/server.js
+++ WAD/W-5/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const songSchema = require("./songDetails.js");
+const songSchema = require("./data.js");
 
 const app = express();
 
